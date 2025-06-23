c728a4ddffef310c66e7f21f8fc2e0b6112984f5
kamilisa225 <142036108+kamilisa225@users.noreply.github.com>
2025-06-23 09:48:36 +0700

    Kendaraan.java

--- platpay/models/Kendaraan.java
+++ platpay/models/Kendaraan.java
@@ -8,10 +8,12 @@ public class Kendaraan {
     private String warna;
     private String nikPemilik;
     private String statusPajak;
+    private double hargaPajak;
+    private String tjtp;
 
     public Kendaraan(String nopol, int tahunKeluar, String merkTipe, 
                     int kapasitasCC, String warna, String nikPemilik, 
-                    String statusPajak) {
+                    String statusPajak, double hargaPajak, String tjtp) {
         this.nopol = nopol;
         this.tahunKeluar = tahunKeluar;
         this.merkTipe = merkTipe;
@@ -19,6 +21,8 @@ public class Kendaraan {
         this.warna = warna;
         this.nikPemilik = nikPemilik;
         this.statusPajak = statusPajak;
+        this.hargaPajak = hargaPajak;
+        this.tjtp = tjtp;
     }
 
     public String getNopol() { return nopol; }
@@ -28,5 +32,9 @@ public class Kendaraan {
     public String getWarna() { return warna; }
     public String getNikPemilik() { return nikPemilik; }
     public String getStatusPajak() { return statusPajak; }
+    public double getHargaPajak() { return hargaPajak; }
+    public String getTjtp() { return tjtp; }
     public void setStatusPajak(String status) { this.statusPajak = status; }
+    public void setHargaPajak(double hargaPajak) { this.hargaPajak = hargaPajak; }
+    public void setTjtp(String tjtp) { this.tjtp = tjtp; }
 }
